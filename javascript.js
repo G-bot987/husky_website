730de4d58a14f0608b71e35a398228e9b4a74138
Georgia <ghobs987@gmail.com>
2021-05-26 20:22:12 +0100

    initialproject

---
+++ javascript.js
@@ -0,0 +1,16 @@
+$("document").ready(function(){
+    
+    $("#1").on("click", function() {
+        // Increase the size of hsk1.
+        $("#hsk1").animate({height:"+=20px", width:"+=20px"}, "fast");
+        // decrease size of hsk2
+        $("#hsk2").animate({height:"-=20px", width:"-=20px"}, "fast");
+    })
+    $("#2").on("click", function() {
+        //decrease size of hsk1
+        $("#hsk1").animate({height:"-=20px", width:"-=20px"}, "fast");
+        //increase size of hsk2
+        $("#hsk2").animate({height:"+=20px", width:"+=20px"}, "fast");
+    })
+
+})
